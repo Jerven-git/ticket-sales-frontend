6f6b87ef0dc96eafe707f46f09191068b0e05a0f
Jerven Latayada <latayada1233@gmail.com>
2024-07-07 15:23:50 +0800

    Sign up and navigation

---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-black min-h-screen flex flex-col">
+    <!-- <NavBar /> -->
+    <div class="flex-grow flex items-center justify-center">
+      <div class="text-white">Welcome to the Home Page</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+// import NavBar from './NavBar.vue';
+
+export default defineComponent({
+  name: 'HomePage',
+  components: {
+    // NavBar,
+  },
+});
+</script>
---
+++ src/router/index.ts
@@ -0,0 +1,33 @@
+ /*It won't change the URL in the browser's address bar. It maintains navigation state internally within the application*/
+import { createMemoryHistory, createRouter } from 'vue-router'
+import UserLogin from '../components/UserLogin.vue';
+import HomePage from '../components/HomePage.vue';
+
+const routes = [
+  { path: '/', component: HomePage, name: 'home' },
+  { path: '/login', component: UserLogin, name: 'login', },
+]
+
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes,
+})
+
+export default router;
+
+ /*This code allows you to reflect the current route and directly access the routes*/
+// import { createRouter, createWebHistory } from 'vue-router'
+// import UserLogin from '../components/UserLogin.vue';
+// import HomePage from '../components/HomePage.vue';
+
+// const routes = [
+//   { path: '/', component: HomePage, name: 'home' },
+//   { path: '/login', component: UserLogin, name: 'login', },
+// ]
+
+// const router = createRouter({
+//   history: createWebHistory(),
+//   routes,
+// })
+
+// export default router;
