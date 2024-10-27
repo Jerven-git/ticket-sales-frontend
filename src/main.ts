36554e929aa75898deb02a8f7c4f273898074b5a
jerven <latayada1233@gmail.com>
2024-10-27 21:35:36 +0800

    changes

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import router from '@/router';
 import axiosPlugin from '@/axios/axios';
 import notifications from '@kyvg/vue3-notification';
 import { useAuthStore } from '@/store/authLogin';
-import axios from 'axios';
 
 const app = createApp(App);
 
@@ -16,7 +15,6 @@ app.use(pinia);
 app.use(router);
 app.use(axiosPlugin);
 app.use(notifications);
-app.provide('axios', axios);
 
 // Extend Pinia with the $notify property
 pinia.use(({ store }) => {
